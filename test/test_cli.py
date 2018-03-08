86f41a076b79805f2433c8842b2530b9233abbd1
Darrick Herwehe <darrick@exitcodeone.com>
2018-03-08 09:41:35 -0500

    test basic headerdoc generation

---
+++ test/test_cli.py
@@ -0,0 +1,18 @@
+# #!/usr/bin/env python3
+from pathlib import Path
+
+import pytest
+
+from asdocs import cli
+
+TEST_PATH = Path(__file__).parent
+
+
+@pytest.mark.integration
+class TestDocumentGeneration:
+	def test_GenerateHeaderdocXml_GivenDocumentedFiles_GeneratesDocumentation(self, tmpdir):
+		source_files_path = TEST_PATH / 'data' / 'source_files'
+		cli.generate_headerdoc_xml(cli.HEADERDOC, source_files_path, tmpdir)
+		xml_files = list(tmpdir.visit(fil='*.xml'))
+		print(xml_files)
+		assert len(xml_files) == 3
